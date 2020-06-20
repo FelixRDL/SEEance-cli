94bb69a9cdadc6be0cfddc8653c608eecfb3d622
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-06-20 18:42:56 +0200

    Implement publish command

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 const generate = require("./generate");
+const publish = require("./publish");
 const yargs = require("yargs");
 
 const cmd = yargs
     .command('generate <what> <name>')
     .command('generate <what> <type> <name>')
     .command('delete <what> <name>')
-    .command('publish <what> [name]')
+    .command('publish <what>')
     .command('configure --token <token> --url <url>')
     .help()
     .argv
@@ -27,6 +28,22 @@ if(operation == 'generate') {
         case 'analysis':
         case 'an':
             generate.generateAnalysis(cmd['name'], '.');
+            break;
+    }
+} else if(operation == 'publish') {
+    const what = cmd['what'];
+    switch(what) {
+        case 'datasource':
+        case 'ds':
+                publish.publish(process.cwd());
+            break;
+        case 'preprocessor':
+        case 'pp':
+            throw new Error("Method not implemented");
+            break;
+        case 'analysis':
+        case 'an':
+            throw new Error("Method not implemented");
             break;
     }
 }
