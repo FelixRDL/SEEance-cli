812a959939eb381b67f73eb3bfc845b0cf404216
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-06-26 15:59:09 +0200

    Remove test folder from tracking

--- .test/gitTestPrep/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function (input, config) {
-  return new Promise((resolve, reject) => {
-    /**
-         * TODO: Implement your preprocessing step here.
-         * */
-    reject(new Error('Method not yet implemented!'))
-  })
-}
